db62340f186c8b4603482f0d33a86d62ac1839da
liamHowatt <liamjmh0@gmail.com>
2025-01-29 14:44:16 -0500

    fix mistake

--- Core/Src/lvgl_port_display.c
+++ Core/Src/lvgl_port_display.c
@@ -23,15 +23,15 @@ void lvgl_display_init (void)
 {
 	/* display initialization */
 
-#if LV_COLOR_FORMAT == 16
+#if LV_COLOR_DEPTH == 16
   static __attribute__((aligned(32))) uint8_t buf_2[MY_DISP_HOR_RES * MY_DISP_VER_RES * 2];
   lv_st_ltdc_create_direct((void *)0x20000000, buf_2, 0);
-#elif LV_COLOR_FORMAT == 24 || LV_COLOR_FORMAT == 32
+#elif LV_COLOR_DEPTH == 24 || LV_COLOR_DEPTH == 32
   static __attribute__((aligned(32))) uint8_t buf_1[MY_DISP_HOR_RES * MY_DISP_VER_RES];
   static __attribute__((aligned(32))) uint8_t buf_2[MY_DISP_HOR_RES * MY_DISP_VER_RES];
   lv_st_ltdc_create_partial(buf_1, buf_2, sizeof(buf_1), 0);
 #else
-  #error LV_COLOR_FORMAT not supported
+  #error LV_COLOR_DEPTH not supported
 #endif
 }
 
